76314a8251a113990d3ca786e77adddcf5d70a4d
Julian <Julian@Julian-PC>
2013-06-01 08:56:48 -0700

    ss

---
+++ src/CWF/StoryBundle/Entity/Code.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace CWF\StoryBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Code
+ */
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="cwf_codes")
+ */
+class Code
+{
+	 private $stories;
+	 
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+	
+	/**
+     * @ORM\Column(type="string", length=5)
+     */
+    protected $shortName;
+	
+    /**
+     * @ORM\Column(type="string", length=10)
+     */
+    protected $name;
+
+   /**
+     * @ORM\Column(type="text")
+     */
+    protected $description;
+
+	
+	public function __construct()
+    {
+        
+        // your own logic
+    }
+	
+
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set shortName
+     *
+     * @param string $shortName
+     * @return Code
+     */
+    public function setShortName($shortName)
+    {
+        $this->shortName = $shortName;
+    
+        return $this;
+    }
+
+    /**
+     * Get shortName
+     *
+     * @return string 
+     */
+    public function getShortName()
+    {
+        return $this->shortName;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Code
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Code
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+}
